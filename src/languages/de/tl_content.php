bc31016bb882edb44b9edf68a45e47c9e79cf09d
Holger Janßen-Kroll <post@ho-jk.de>
2018-11-03 12:02:01 +0100

    added headline legend to translation

--- src/languages/de/tl_content.php
+++ src/languages/de/tl_content.php
@@ -1,6 +1,7 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_content']['event_legend'] = 'Auswahl eines Kalender-Ereignisses';
+$GLOBALS['TL_LANG']['tl_content']['headline_legend'] = 'Überschrift';
 
 $GLOBALS['TL_LANG']['tl_content']['muk_calendar'] = array ( 'Kalender', 'Wählen Sie einen Kalender aus, um fortzufahren!');
 $GLOBALS['TL_LANG']['tl_content']['muk_event'] = array ( 'Event', 'Wählen Sie ein Event aus der Drop-Down-Liste, um die Auswahl abzuschießen und den ersten Text-Inhalt des Events zu kopieren');
